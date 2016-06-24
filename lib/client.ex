718884ff6b72aac51722765aaca79120e3e623cb
Skyler Parr <sparr@avvo.com>
2016-06-24 14:44:05 -0700

    cleaned up the client

--- lib/client.ex
+++ lib/client.ex
@@ -1,37 +1,13 @@
 defmodule Client do
 
-  def connect(false, :undefined) do
-    IO.puts "Connecting to node"
-    connect Node.connect(:"main@10.3.17.68"), :undefined
-  end
-
-  def connect(true, :undefined) do
-    IO.puts "Finding main name"
-    connect true, find_main
-  end
-
-  def connect(true, main) do
-    IO.puts "Sending connect to main"
-    send main, {:connect, :donald, self}
-    wait_for_role
-  end
-
-  def connect do
+  def connect(name) when is_atom(name) do
     IO.puts "Starting up..."
-    connect(false, :undefined)
+    do_connect(name, false, :undefined)
   end
 
-  def wait_for_role do
-    IO.puts "Waiting for role"
-    receive do
-      {:role, role} -> role
-    end
-  end
-
-  def send_agents(agents), do: send find_main, {:agents, agents}
-
-  def send_outcome(:red), do: send await_secret, {:outcome, :red}
-  def send_outcome(:black), do: send await_secret, {:outcome, :black}
+  def send_agents(agents), do: send main_pid, {:agents, agents}
+  def fail_mission, do: send await_secret, {:outcome, :red}
+  def success_mission, do: send await_secret, {:outcome, :black}
 
   def await_secret do
     receive do
@@ -39,7 +15,29 @@ defmodule Client do
     end
   end
 
-  def find_main do
-    :global.whereis_name(:main)
+  def main_pid, do: :global.whereis_name(:main)
+
+  defp do_connect(name, false, :undefined) do
+    IO.puts "Connecting to node"
+    do_connect name, Node.connect(:"main@10.3.17.68"), :undefined
   end
+
+  defp do_connect(name, true, :undefined) do
+    IO.puts "Finding main name"
+    do_connect name, true, main_pid
+  end
+
+  defp do_connect(name, true, main) do
+    IO.puts "Sending connect to main"
+    send main, {:connect, name, self}
+    wait_for_role
+  end
+
+  defp wait_for_role do
+    IO.puts "Waiting for role"
+    receive do
+      {:role, role} -> role
+    end
+  end
+
 end
